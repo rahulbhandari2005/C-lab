41914690d019980e6e18eeb005a212989065d6e8
rahulbhandari2005 <rahulbhandari1250@gmail.com>
2025-09-03 12:23:38 +0530

    Add files via upload

---
+++ sum of 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=0;
+	int c=0;
+	int b=0;
+	printf("enter 2 numbers \n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a+b;
+	printf("sum of two number is %d",c);
+	return 0;
+}
